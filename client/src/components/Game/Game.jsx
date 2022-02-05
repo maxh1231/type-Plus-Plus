c2cc7d67e4256f186b7e16fb736ab743b6455187
Leithen <leithen113@gmail.com>
2022-02-04 20:44:35 -0700

    Added accuracy tracker

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -2,20 +2,21 @@ import React, { useState } from 'react';
 
 function Game() {
     const [validInput, setValidInput] = useState(true);
-    let [errorCount, setErrorCount] = useState(0);
+    const [errorCount, setErrorCount] = useState(0);
+    const [accuracy, setAccuracy] = useState(100);
     const sampleText = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus vel consequuntur rerum distinctio exercitationem architecto ex debitis hic nobis necessitatibus cum animi, quod saepe maxime reprehenderit culpa nisi voluptates labore!";
     let sampleArr = sampleText.split('');
     const arrLength = sampleArr.length;
     function userInput(evt) {
         let input = evt.target.value;
-        let count = 0;
+        let errorCount = 0;
         for (let i = 0; i < input.length; i++) {
             document.getElementById(i).style.textDecoration = 'underline';
             if (input[i] !== sampleArr[i]) {
                 document.getElementById(i).style.color = 'red';
                 console.log(`Wrong at position ${i}`);
                 setValidInput(false);
-                count++
+                errorCount++
             } else {
                 document.getElementById(i).style.color = 'green';
                 setValidInput(true);
@@ -23,8 +24,10 @@ function Game() {
         }
         for (let i = input.length; i < arrLength; i++) {
             document.getElementById(i).style.textDecoration = 'none';
+            document.getElementById(i).style.color = 'black';
         }
-        setErrorCount(count);
+        setErrorCount(errorCount);
+        setAccuracy(Math.abs(errorCount / input.length * 100 - 100));
     }
 
     return (
@@ -32,12 +35,12 @@ function Game() {
             {sampleArr.map((char, i) => (
                 <span key={i} id={i}>{char}</span>
             ))}
-            {/* <p>{sampleText}</p> */}
             {!validInput && 
                 <p>Incorrect!</p>
             }
             <textarea id="gameInput" rows="4" cols="50" onChange={userInput} style={{ display: "block"}}></textarea>
             <p>Errors: {errorCount}</p>
+            <p>Accuracy: {accuracy}%</p>
         </div>
     )
 }
