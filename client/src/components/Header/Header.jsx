7d06abf13905b4382cc0d1d6c2ef936cb9174e12
Ben H <deepfreespace@gmail.com>
2022-02-07 18:52:27 -0700

    Footer skeleton

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
 const Header = () => {
-    const logout = event => {
+    const logout = (event) => {
         event.preventDefault();
         Auth.logout();
     };
