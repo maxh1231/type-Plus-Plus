242860aaf20a6477497bd8217e20a85cae774ede
maxh1231 <maxhumpherys@gmail.com>
2022-02-07 17:20:21 -0700

    user info on dashboard

--- client/src/components/UserInfo/UserInfo.jsx
+++ client/src/components/UserInfo/UserInfo.jsx
@@ -4,15 +4,28 @@ const UserInfo = ({ data }) => {
     return (
         <section>
             <div>
-                <img src={defaultPhoto} alt=''></img>
+                <img src={defaultPhoto} alt='' width='100' height='100'></img>
             </div>
             <div>
                 <h3>Hello {data.me.username}</h3>
             </div>
+            {/* Bio */}
             <div>
                 <p>
-                    {/* Bio section(?) */}
+                    {data.me.bio}
                 </p>
+
+                {!data.me.bio && <span>Add a bio!</span>}
+
+                {data.me.bio && <span>Update bio</span>}
+            </div>
+            {/* Test results */}
+            <div>
+                <p>Highest WPM: 105</p>
+                <p>Average WPM: 87</p>
+            </div>
+            <div>
+                <p>Location: United States </p>
             </div>
         </section>
     )
