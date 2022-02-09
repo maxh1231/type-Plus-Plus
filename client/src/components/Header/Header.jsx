9d94360cbf293709abb2d05e9923be543c966568
Ben H <deepfreespace@gmail.com>
2022-02-08 17:11:32 -0700

    Links merged into navbar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -11,22 +11,85 @@ const Header = () => {
     return (
         <header>
             <nav className="relative w-full flex flex-wrap items-center justify-around py-4 bg-gray-100 text-gray-600 shadow-md navbar navbar-expand-lg navbar-light hover:text-gray-800 focus:text-gray-800 ">
-                <Link to="/">
-                    <h2>Home</h2>
-                </Link>
-                {Auth.loggedIn() ? (
-                    <>
-                        <Link to="/dashboard">Dashboard</Link>
-                        <a href="/" onClick={logout}>
-                            Logout
-                        </a>
-                    </>
-                ) : (
-                    <>
-                        <Link to="/login">Login</Link>
-                        <Link to="/signup">Signup</Link>
-                    </>
-                )}
+                <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+                    <button
+                        className="navbar-toggler text-gray-500 border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <svg
+                            aria-hidden="true"
+                            focusable="false"
+                            data-prefix="fas"
+                            data-icon="bars"
+                            class="w-6"
+                            role="img"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+                            ></path>
+                        </svg>
+                    </button>
+                    <div
+                        className="collapse navbar-collapse flex-grow items-center"
+                        id="navbarSupportedContent"
+                    >
+                        <ul className="navbar-nav flex pl-0 list-style-none mr-auto">
+                            <li className="nav-item p-2">
+                                <Link to="/" className="nav-link p-0">
+                                    <h2>Home</h2>
+                                </Link>
+                            </li>
+                            {Auth.loggedIn() ? (
+                                <>
+                                    <li className="nav-item p-2">
+                                        <Link
+                                            to="/dashboard"
+                                            className="nav-link p-0"
+                                        >
+                                            Dashboard
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item p-2">
+                                        <a
+                                            href="/"
+                                            className="nav-link p-0"
+                                            onClick={logout}
+                                        >
+                                            Logout
+                                        </a>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li className="nav-item p-2">
+                                        <Link
+                                            to="/login"
+                                            className="nav-link p-0"
+                                        >
+                                            Login
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item p-2">
+                                        <Link
+                                            to="/signup"
+                                            className="nav-link p-0"
+                                        >
+                                            Sign Up
+                                        </Link>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </header>
     );
