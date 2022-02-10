4d8ccea7b7d85d1afdcec7aad81c3a0843ebb753
Leithen <leithen113@gmail.com>
2022-02-09 23:13:00 -0700

    Conditionally rendering chart data with user query

--- client/src/components/Chart/Chart.jsx
+++ client/src/components/Chart/Chart.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
+import { QUERY_MYSCORE } from '../../utils/queries';
+import Auth from '../../utils/auth';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from 'chart.js';
 
 ChartJS.register( CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler );
 
 const Chart = () => {
+    const { loading, data } = useQuery(QUERY_MYSCORE);
+    let wpm = 0;
+    let accuracy = 0;
+    
+    if (Auth.loggedIn()) {
+        console.log(data)
+        wpm = data?.scoresByUser.map(score => { return score.wpm })
+        accuracy =  data?.scoresByUser.map(score => { return score.accuracy })
+    } 
+
     return (
         <>
             <Line 
                 data={{
-                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    labels: [],
                     datasets: [
                         {
                             label: 'Words Per Minute',
-                            data: [20, 30, 30, 35, 40, 50],
+                            data: {...wpm},
                             borderColor: 'rgb(255, 99, 132)',
                             backgroundColor: 'rgba(255, 99, 132, 0.5)',
                             fill: true
                         }, {
                             label: 'Accuracy',
-                            data: [80, 85, 87, 90, 90, 95],
+                            data: {...accuracy},
                             borderColor: 'rgb(53, 162, 235)',
                             backgroundColor: 'rgba(53, 162, 235, 0.5)',
                             fill: true
