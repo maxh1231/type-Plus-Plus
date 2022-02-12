22863c0a305d6e4641fd13590bf75faaa356581a
Leithen <leithen113@gmail.com>
2022-02-12 12:15:12 -0700

    Updated leaderboard styling slightly:

--- client/src/components/GlobalLeaderBoard/GlobalLeaderBoard.jsx
+++ client/src/components/GlobalLeaderBoard/GlobalLeaderBoard.jsx
@@ -10,9 +10,9 @@ const GlobalLeaderBoard = () => {
     const leaderBoard = data?.scores.map(score => { return {wpm: score.wpm, accuracy: score.accuracy, username: score.username, date: formatTime(score.createdAt)} })
 
     return (
-        <>
-            <h1 className='block text-center text-2xl'>Global Leaderboard</h1>
-            <table className='table-auto'>
+        <section className='w-3/4 mx-auto my-4'>
+            <h1 className='block text-center text-2xl underline'>Global Leaderboard</h1>
+            <table className='table-auto mx-auto'>
                 <thead>
                     <tr>
                         <th>#</th>
@@ -26,9 +26,9 @@ const GlobalLeaderBoard = () => {
                     <tbody>
                         {leaderBoard.map((score, i) => (
                             <tr key={uuid()}>
-                                <td key={uuid()}>{i + 1}</td>
-                                <td key={uuid()}>{score.wpm}</td>
-                                <td key={uuid()}>
+                                <td className='text-center p-2' key={uuid()}>{i + 1}</td>
+                                <td className='text-center p-2' key={uuid()}>{score.wpm}</td>
+                                <td className='text-center p-2' key={uuid()}>
                                     {i == 0 && 
                                         <img className='inline m1' src="https://img.icons8.com/external-yogi-aprelliyanto-flat-yogi-aprelliyanto/28/000000/external-medal-award-yogi-aprelliyanto-flat-yogi-aprelliyanto.png"/>
                                     } {i == 1 && 
@@ -37,8 +37,8 @@ const GlobalLeaderBoard = () => {
                                         <img className='inline m1' src="https://img.icons8.com/external-yogi-aprelliyanto-flat-yogi-aprelliyanto/28/000000/external-medal-award-yogi-aprelliyanto-flat-yogi-aprelliyanto-13.png"/>}
                                     {score.username}
                                 </td>
-                                <td key={uuid()}>{score.accuracy}</td>
-                                <td key={uuid()}>{score.date}</td>
+                                <td className='text-center p-2' key={uuid()}>{score.accuracy}</td>
+                                <td className='text-center p-2' key={uuid()}>{score.date}</td>
                             </tr>
                         ))}
                     </tbody>
@@ -50,7 +50,7 @@ const GlobalLeaderBoard = () => {
                     </tbody>
                 )}
             </table>
-        </>
+        </section>
     )
 }
 
--- client/src/components/WeeklyLeaderBoard/WeeklyLeaderBoard.jsx
+++ client/src/components/WeeklyLeaderBoard/WeeklyLeaderBoard.jsx
@@ -10,9 +10,9 @@ const WeeklyLeaderBoard = () => {
     const leaderBoard = data?.weeklyScores.map(score => { return {wpm: score.wpm, accuracy: score.accuracy, username: score.username, date: formatTime(score.createdAt)} })
 
     return (
-        <>
-            <h1 className='block text-center text-2xl'>Weekly Leaderboard</h1>
-            <table className='table-auto'>
+        <section  className='w-3/4 mx-auto my-4'>
+            <h1 className='block text-center text-2xl underline'>Weekly Leaderboard</h1>
+            <table className='table-auto mx-auto'>
                 <thead>
                     <tr>
                         <th>#</th>
@@ -26,9 +26,9 @@ const WeeklyLeaderBoard = () => {
                     <tbody>
                         {leaderBoard.map((score, i) => (
                             <tr key={uuid()}>
-                                <td className='text-center' key={uuid()}>{i + 1}</td>
-                                <td className='text-center' key={uuid()}>{score.wpm}</td>
-                                <td className='text-center' key={uuid()}>
+                                <td className='text-center p-2' key={uuid()}>{i + 1}</td>
+                                <td className='text-center p-2' key={uuid()}>{score.wpm}</td>
+                                <td className='text-center p-2' key={uuid()}>
                                     {i == 0 && 
                                         <img className='inline m1' src="https://img.icons8.com/external-yogi-aprelliyanto-flat-yogi-aprelliyanto/28/000000/external-medal-award-yogi-aprelliyanto-flat-yogi-aprelliyanto.png"/>
                                     } {i == 1 && 
@@ -37,8 +37,8 @@ const WeeklyLeaderBoard = () => {
                                         <img className='inline m1' src="https://img.icons8.com/external-yogi-aprelliyanto-flat-yogi-aprelliyanto/28/000000/external-medal-award-yogi-aprelliyanto-flat-yogi-aprelliyanto-13.png"/>}
                                     {score.username}
                                 </td>
-                                <td className='text-center' key={uuid()}>{score.accuracy}</td>
-                                <td className='text-center' key={uuid()}>{score.date}</td>
+                                <td className='text-center p-2' key={uuid()}>{score.accuracy}</td>
+                                <td className='text-center p-2' key={uuid()}>{score.date}</td>
                             </tr>
                         ))}
                     </tbody>
@@ -50,7 +50,7 @@ const WeeklyLeaderBoard = () => {
                     </tbody>
                 )}
             </table>
-        </>
+        </section>
     )
 }
 
