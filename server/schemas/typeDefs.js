fc275b27d21b9fcb45c1ff012d624d34794ef82e
Leithen <leithen113@gmail.com>
2022-02-12 15:54:55 -0700

    Fixing merge conflict

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,49 +2,52 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type User {
-        _id: ID
-        username: String!
-        email: String!
-        createdAt: String!
-        bio: String
-        location: String
-        scoreCount: Int
-        scores: [Scores]
-        friendCount: Int
-        friends: [User]
+      _id: ID
+      username: String!
+      email: String!
+      createdAt: String!
+      bio: String
+      location: String
+      scoreCount: Int
+      scores: [Scores]
+      friendCount: Int
+      friends: [User]
     }
 
     type Scores {
-        _id: ID
-        wpm: Float!
-        accuracy: Float
-        username: String
-        createdAt: String
+      _id: ID
+      wpm: Float!
+      accuracy: Float
+      username: String
+      createdAt: String
     }
 
     type Auth {
-        token: ID!
-        user: User
+      token: ID!
+      user: User
     }
 
-    type Query {
-        me: User
-        meScores: [Scores]
-        users: [User]
-        user(username: String!): User
-        scoresByUser(username: String): [Scores]
-        scores: [Scores]
-        weeklyScores: [Scores]
-    }
+  type Query {
+    me: User
+    meScores: [Scores]
+    users: [User]
+    user(username: String!): User
+    scoresByUser(username: String): [Scores]
+    scores: [Scores]
+    weeklyScores: [Scores]
+    badge: [ Badge ]
+  }
 
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        addScore(wpm: Float!, accuracy: Float!): Scores
-        addBio(bio: String!): User
-        addLocation(location: String!): User
-        addFriend(friendId: ID!): User
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addScore(wpm: Float!, accuracy: Float! ): Scores
+    addBio(bio: String!): User
+    addLocation(location: String!): User
+    addFriend(friendId: ID!): User
+    addBadge(badgeName: String!): Badge
+    createBadge(badgeName: String!): Badge
+  }
 `;
 
 module.exports = typeDefs;
