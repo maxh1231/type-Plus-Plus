5b118610dcf906cccad1227faf5ec21f302b4d23
Leithen <leithen113@gmail.com>
2022-02-12 12:05:16 -0700

    Created weekly leader board component

---
+++ client/src/components/WeeklyLeaderBoard/WeeklyLeaderBoard.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { v4 as uuid } from 'uuid';
+import { useQuery } from '@apollo/client';
+import { QUERY_WEEKLY_SCORES } from '../../utils/queries';
+import { formatTime } from '../../utils/helpers';
+
+const WeeklyLeaderBoard = () => {
+    const { loading, data } = useQuery(QUERY_WEEKLY_SCORES);
+    
+    const leaderBoard = data?.weeklyScores.map(score => { return {wpm: score.wpm, accuracy: score.accuracy, username: score.username, date: formatTime(score.createdAt)} })
+
+    return (
+        <>
+            <h1 className='block text-center text-2xl'>Weekly Leaderboard</h1>
+            <table className='table-auto'>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>WPM</th>
+                        <th>User</th>
+                        <th>Accuracy</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                {leaderBoard ? (
+                    <tbody>
+                        {leaderBoard.map((score, i) => (
+                            <tr key={uuid()}>
+                                <td className='text-center' key={uuid()}>{i + 1}</td>
+                                <td className='text-center' key={uuid()}>{score.wpm}</td>
+                                <td className='text-center' key={uuid()}>
+                                    {i == 0 && 
+                                        <img className='inline m1' src="https://img.icons8.com/external-yogi-aprelliyanto-flat-yogi-aprelliyanto/28/000000/external-medal-award-yogi-aprelliyanto-flat-yogi-aprelliyanto.png"/>
+                                    } {i == 1 && 
+                                        <img className='inline m1' src="https://img.icons8.com/external-yogi-aprelliyanto-flat-yogi-aprelliyanto/28/000000/external-medal-award-yogi-aprelliyanto-flat-yogi-aprelliyanto-2.png"/>
+                                    } {i == 2 && 
+                                        <img className='inline m1' src="https://img.icons8.com/external-yogi-aprelliyanto-flat-yogi-aprelliyanto/28/000000/external-medal-award-yogi-aprelliyanto-flat-yogi-aprelliyanto-13.png"/>}
+                                    {score.username}
+                                </td>
+                                <td className='text-center' key={uuid()}>{score.accuracy}</td>
+                                <td className='text-center' key={uuid()}>{score.date}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                ) : (
+                    <tbody>
+                        <tr>
+                            <td>Loading...</td>
+                        </tr>
+                    </tbody>
+                )}
+            </table>
+        </>
+    )
+}
+
+export default WeeklyLeaderBoard
