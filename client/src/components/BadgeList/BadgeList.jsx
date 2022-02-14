25143f05d7b3de560a6261e3f9270c791bf6b6c4
Leithen <leithen113@gmail.com>
2022-02-13 21:25:30 -0700

    Separating earned vs unearned badges

--- client/src/components/BadgeList/BadgeList.jsx
+++ client/src/components/BadgeList/BadgeList.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
 import { v4 as uuid } from 'uuid';
 import { useQuery } from '@apollo/client';
-import { QUERY_BADGES } from '../../utils/queries';
+import { QUERY_BADGES, QUERY_MYBADGE } from '../../utils/queries';
 import { ViewGridIcon, ViewListIcon } from '@heroicons/react/outline';
 
 
 const BadgeList = () => {
     const [viewGrid, setViewGrid] = useState(true);
     const { loading, data } = useQuery(QUERY_BADGES);
+    const myBadgeData = useQuery(QUERY_MYBADGE);
     const badgeArr = data?.badges || [];
+    const myBadgeArr = myBadgeData.data?.meBadges.badges || [];
+    let tmpArr = [...badgeArr]
+    for (let i = 0; i < badgeArr.length - 1; i++) {
+        for (let j = 0; j < myBadgeArr.length; j++) {
+            if (badgeArr[i]._id === myBadgeArr[j]._id) {
+                tmpArr.splice(i, 1);
+            }
+        }
+    }
+    console.log(badgeArr, myBadgeArr, tmpArr)
     const setGrid = () => {
         setViewGrid(true);
     };
@@ -22,24 +33,50 @@ const BadgeList = () => {
                 <button className='m-2 float-right bg-gray-100' onClick={setList}><ViewListIcon className='h-5 w-5 m-2 inline'/></button>
             </div>
             {viewGrid ? (
-                <div className='container flex flex-wrap w-full'>
-                    {badgeArr.map(badge => (
-                        <div id='card' key={uuid()} className='border rounded-md p-2 m-2'>
-                            <img src={`.${badge.img}`} key={uuid()} className='m-auto'></img>
-                            <p key={uuid()} className='p-1 text-center font-bold'>{badge.badgeName}</p>
-                            <p key={uuid()} className='p-1 text-center italic'>{badge.description}</p>
-                        </div>
-                    ))}
+                <div className='flex flex-wrap w-full'>
+                    <div className='flex flex-wrap w-full justify-center'>
+                        <p className='text-xl font-bold text-center w-full'>Earned</p>
+                        {myBadgeArr.map(badge => (
+                            <div id='card' key={uuid()} className='border rounded-md p-2 m-2'>
+                                <img src={`.${badge.img}`} key={uuid()} className='m-auto'></img>
+                                <p key={uuid()} className='p-1 text-center font-bold'>{badge.badgeName}</p>
+                                <p key={uuid()} className='p-1 text-center italic'>{badge.description}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <div className='flex flex-wrap w-full justify-center'>
+                        <p className='text-xl font-bold text-center w-full'>Not Yet Earned</p>
+                        {tmpArr.map(badge => (
+                            <div id='card' key={uuid()} className='border rounded-md p-2 m-2'>
+                                <img src={`.${badge.img}`} key={uuid()} className='m-auto'></img>
+                                <p key={uuid()} className='p-1 text-center font-bold'>{badge.badgeName}</p>
+                                <p key={uuid()} className='p-1 text-center italic'>{badge.description}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             ) : (                
-                <div className='container'>
-                    {badgeArr.map(badge => (
-                        <div id='card' key={uuid()} className='border rounded-md p-2 m-2'>
-                            <img src={`.${badge.img}`} key={uuid()} className='p-2 inline border-r-2'></img>
-                            <p key={uuid()} className='p-2 inline text-center font-bold'>{badge.badgeName}</p>
-                            <p key={uuid()} className='p-2 inline text-center italic'>{badge.description}</p>
-                        </div>
-                    ))}
+                <div className='w-full'>
+                    <div className='w-full justify-center'>
+                        <p className='text-xl font-bold m-2'>Earned</p>
+                        {myBadgeArr.map(badge => (
+                            <div id='card' key={uuid()} className='border rounded-md p-2 m-2'>
+                                <img src={`.${badge.img}`} key={uuid()} className='m-auto p-2 inline border-r'></img>
+                                <p key={uuid()} className='p-2 text-center font-bold inline'>{badge.badgeName}</p>
+                                <p key={uuid()} className='p-2 text-center italic inline'>{badge.description}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <div className='w-full justify-center'>
+                        <p className='text-xl font-bold m-2'>Not Yet Earned</p>
+                        {tmpArr.map(badge => (
+                            <div id='card' key={uuid()} className='border rounded-md p-2 m-2'>
+                                <img src={`.${badge.img}`} key={uuid()} className='m-auto p-2 inline border-r'></img>
+                                <p key={uuid()} className='p-2 text-center font-bold inline'>{badge.badgeName}</p>
+                                <p key={uuid()} className='p-2 text-center italic inline'>{badge.description}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             )}
         </section>
