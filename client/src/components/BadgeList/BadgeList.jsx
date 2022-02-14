5cb461ae122c1426cc43585d8f164622ab488c23
Leithen <leithen113@gmail.com>
2022-02-13 19:50:19 -0700

    Badge list toggles between grid and list view

--- client/src/components/BadgeList/BadgeList.jsx
+++ client/src/components/BadgeList/BadgeList.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { v4 as uuid } from 'uuid';
 import { useQuery } from '@apollo/client';
 import { QUERY_BADGES } from '../../utils/queries';
+import { ViewGridIcon, ViewListIcon } from '@heroicons/react/outline';
+
 
 const BadgeList = () => {
+    const [viewGrid, setViewGrid] = useState(true);
     const { loading, data } = useQuery(QUERY_BADGES);
     const badgeArr = data?.badges || [];
-    
+    const setGrid = () => {
+        setViewGrid(true);
+    };
+    const setList = () => {
+        setViewGrid(false);
+    };
     return (
         <section className='w-full'>
-            <div className='container flex flex-wrap w-full'>
+            <div className='h-12 m-1 p-1'>
+                <button className='m-2 float-right bg-gray-100' onClick={setGrid}><ViewGridIcon className='h-5 w-5 m-2 inline'/></button>
+                <button className='m-2 float-right bg-gray-100' onClick={setList}><ViewListIcon className='h-5 w-5 m-2 inline'/></button>
+            </div>
+            {viewGrid ? (
+                <div className='container flex flex-wrap w-full'>
                     {badgeArr.map(badge => (
                         <div id='card' key={uuid()} className='border rounded-md p-2 m-2'>
                             <img src={`.${badge.img}`} key={uuid()} className='m-auto'></img>
@@ -17,7 +30,18 @@ const BadgeList = () => {
                             <p key={uuid()} className='p-1 text-center italic'>{badge.description}</p>
                         </div>
                     ))}
-            </div>
+                </div>
+            ) : (                
+                <div className='container'>
+                    {badgeArr.map(badge => (
+                        <div id='card' key={uuid()} className='border rounded-md p-2 m-2'>
+                            <img src={`.${badge.img}`} key={uuid()} className='p-2 inline border-r-2'></img>
+                            <p key={uuid()} className='p-2 inline text-center font-bold'>{badge.badgeName}</p>
+                            <p key={uuid()} className='p-2 inline text-center italic'>{badge.description}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </section>
     )
 }
