9674ebb596fa242d7b7361e8df5cdba3919f62ed
maxh1231 <maxhumpherys@gmail.com>
2022-02-13 21:10:19 -0700

    client side uploader

--- client/src/components/Uploader/Uploader.jsx
+++ client/src/components/Uploader/Uploader.jsx
@@ -1,32 +1,66 @@
 import React from 'react';
 
 const Uploader = ({ image, setImage, url, setUrl }) => {
-    const uploadImage = () => {
-        const data = new FormData()
-        data.append("file", image)
-        data.append("upload_preset", "mhpreset")
-        data.append("cloud_name", "djevcwcbq")
-        fetch("  https://api.cloudinary.com/v1_1/djevcwcbq/image/upload", {
-            method: "post",
-            body: data
-        })
-            .then(resp => resp.json())
-            .then(data => {
-                setUrl(data.url)
-            })
-            .catch(err => console.log(err))
-    }
+    // const uploadImage = () => {
+    //     const data = new FormData()
+    //     data.append("file", image)
+    //     data.append("upload_preset", "mhpreset")
+    //     data.append("cloud_name", "djevcwcbq")
+    //     fetch("  https://api.cloudinary.com/v1_1/djevcwcbq/image/upload", {
+    //         method: "post",
+    //         body: data
+    //     })
+    //         .then(resp => resp.json())
+    //         .then(data => {
+    //             setUrl(data.url)
+    //         })
+    //         .catch(err => console.log(err))
+    // }
+
+    // return (
+    //     <>
+    //         <div>
+    //             <input type="file" onChange={(e) => setImage(e.target.files[0])}></input>
+    //             <button onClick={uploadImage}>Upload</button>
+    //         </div>
+    //         {image && <div className="w-[200px] h-[200px]">
+    //             {<img className="w-full h-full object-cover" src={url} alt="" />}
+    //         </div>}
+    //     </>
+    // )
 
     return (
-        <>
+        <section>
+
+
             <div>
-                <input type="file" onChange={(e) => setImage(e.target.files[0])}></input>
-                <button onClick={uploadImage}>Upload</button>
+                <h1>To Upload Image on mongoDB</h1>
+                <form action="/" method="POST" encType="multipart/form-data">
+                    <div>
+                        <label htmlFor="name">Image Title</label>
+                        <input type="text" id="name" placeholder="Name"
+                            name="name" required></input>
+                    </div>
+                    <div>
+                        <label htmlFor="desc">Image Description</label>
+                        <textarea id="desc" name="desc" rows="2"
+                            placeholder="Description" required>
+                        </textarea>
+                    </div>
+                    <div>
+                        <label htmlFor="image">Upload Image</label>
+                        <input type="file" id="image"
+                            name="image" required></input>
+                    </div>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
             </div>
-            {image && <div className="w-[200px] h-[200px]">
-                {<img className="w-full h-full object-cover" src={url} alt="" />}
-            </div>}
-        </>
+
+
+
+        </section>
     )
 
 }
--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const multer = require('multer');
+const Image = require('./models');
 
 const server = new ApolloServer({
     typeDefs,
@@ -23,6 +25,19 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.set("view engine", "ejs");
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+var upload = multer({ storage: storage });
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -33,6 +48,39 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(require('./controllers'));
+
+app.get('/', (req, res) => {
+    Image.find({}, (err, items) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('An error occurred', err);
+        }
+        else {
+            res.render('imagesPage', { items: items });
+        }
+    });
+});
+
+app.post('/', upload.single('image'), (req, res, next) => {
+
+    var obj = {
+        name: req.body.name,
+        desc: req.body.desc,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    }
+    Image.create(obj, (err, item) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // item.save();
+            res.redirect('/');
+        }
+    });
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
