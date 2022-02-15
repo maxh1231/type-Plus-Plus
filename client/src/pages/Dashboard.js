cbd11faa228caa794f2793c077673a39d758f13d
Leithen <leithen113@gmail.com>
2022-02-14 17:18:58 -0700

    Working on heroku bugs

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -56,9 +56,9 @@ const Dashboard = () => {
         return <div>Loading...</div>;
     }
 
-    if (!user?.username) {
+    if (!Auth.loggedIn()) {
         return <h4 className="flex-grow">Must be logged in</h4>;
-    }
+    } else {
 
     return (
         <main className="flex-grow">
@@ -104,5 +104,5 @@ const Dashboard = () => {
         </main>
     );
 };
-
+}
 export default Dashboard;
