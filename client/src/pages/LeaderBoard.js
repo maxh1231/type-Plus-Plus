3dc912fd1dc059bc4642644daa2a502728c78275
Ben H <deepfreespace@gmail.com>
2022-02-15 12:23:46 -0700

    Fixed sticky footer for profile, leaderboard pages

--- client/src/pages/LeaderBoard.js
+++ client/src/pages/LeaderBoard.js
@@ -1,15 +1,15 @@
-import GlobalLeaderBoard from "../components/GlobalLeaderBoard/GlobalLeaderBoard"
-import ActivityLeaderBoard from "../components/ActivityLeaderBoard/ActivityLeaderBoard"
-import WeeklyLeaderBoard from "../components/WeeklyLeaderBoard/"
+import GlobalLeaderBoard from '../components/GlobalLeaderBoard/GlobalLeaderBoard';
+import ActivityLeaderBoard from '../components/ActivityLeaderBoard/ActivityLeaderBoard';
+import WeeklyLeaderBoard from '../components/WeeklyLeaderBoard/';
 
 const LeaderBoard = () => {
     return (
-        <>
+        <main className="flex-grow">
             <GlobalLeaderBoard />
             <ActivityLeaderBoard />
             <WeeklyLeaderBoard />
-        </>
-    )
-}
+        </main>
+    );
+};
 
-export default LeaderBoard
+export default LeaderBoard;
