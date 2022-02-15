395dc98c8f2302ff203d8ba45d6b09395b7f7446
maxh1231 <maxhumpherys@gmail.com>
2022-02-15 13:48:02 -0700

    pencil icon for updating dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -17,6 +17,8 @@ import Chart from '../components/Chart'
 
 import defaultPhoto from '../assets/images/no-profile-picture.svg'
 
+import { pencil } from '@heroicons/react/solid'
+
 
 // Modal Styles, remove later for custom styles
 const customStyles = {
@@ -85,7 +87,9 @@ const Dashboard = () => {
                     image={image}
                     setImage={setImage}
                 />
-                <button onClick={openModal}>Edit Profile</button>
+                <svg onClick={openModal} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                </svg>
             </div>
 
 
