580745785c7d45cc893251907be4510a2897cf36
Leithen <leithen113@gmail.com>
2022-02-14 19:16:06 -0700

    Fixing typos

--- client/src/components/RecentBadge/RecentBadge.jsx
+++ client/src/components/RecentBadge/RecentBadge.jsx
@@ -16,7 +16,7 @@ const RecentBadge = () => {
                     <p className='text-center'>{activeBadge.description}</p>
                 </div>
             ) : (
-                <p>No badges yes!</p>
+                <p>No badges yet!</p>
             )}
         </div>
     )
