19eedf0c4d4c40d9a395bd34a8e15b4f470efca6
Ben H <deepfreespace@gmail.com>
2022-02-16 17:02:51 -0700

    Final layout of homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -43,6 +43,34 @@ const Home = () => {
 
     return (
         <main className="flex-grow flex flex-col content-around justify-evenly items-center">
+            {/* Intro text */}
+            <section className="container">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque orci mi, dapibus in volutpat ac, bibendum vel
+                    dui. Duis lacinia odio tincidunt justo commodo, vel feugiat
+                    elit molestie. Mauris ut convallis elit. Sed interdum, dui
+                    faucibus faucibus consequat, odio magna ultricies neque, et
+                    dictum nisi nisl et eros. Pellentesque posuere et metus in
+                    accumsan. Ut mollis dui eu consectetur mattis. Nam tempor
+                    consequat semper. Pellentesque consectetur consectetur urna.
+                    Vestibulum interdum mauris justo, id vulputate enim iaculis
+                    sit amet.
+                </p>
+                <p>
+                    Donec quis felis sagittis, scelerisque ante sit amet,
+                    bibendum neque. Nulla facilisi. Cras iaculis ex velit, nec
+                    consectetur nulla suscipit eget. Ut imperdiet est quis ipsum
+                    condimentum, non finibus arcu venenatis. Cras pretium
+                    accumsan diam, in auctor dolor elementum ut. Mauris pulvinar
+                    quam in ultrices suscipit. Donec laoreet mi nunc, vel
+                    aliquet tortor convallis fermentum. Phasellus rhoncus auctor
+                    nisi et vestibulum. Nam scelerisque ipsum quis urna posuere
+                    condimentum. Nulla quis facilisis nulla. Sed lacinia
+                    accumsan diam, id ultrices quam laoreet at. Sed sit amet
+                    varius nibh. Nullam quis sodales lacus.
+                </p>
+            </section>
             <section className="m-4">
                 {!runGame && (
                     <button
@@ -73,17 +101,19 @@ const Home = () => {
                                 <p>Loading...</p>
                             )}
                         </div>
-                        <Game sampleArr={sampleArr} unmount={endGame} loggedIn={loggedIn} />
+                        <Game
+                            sampleArr={sampleArr}
+                            unmount={endGame}
+                            loggedIn={loggedIn}
+                        />
                     </>
                 )}
             </section>
-            <div className="grid grid-cols-2 w-full">
-                <div className="container">
-                    <Chart />
-                </div>
-                <div className="container">
+            <div className="grid grid-cols-3 container">
+                <div className="">
                     <GlobalLeaderBoard />
                 </div>
+                <div className="col-span-2 bg-gray-400">Badge Progress</div>
             </div>
             {/* {loggedIn && <RecentBadge />} */}
         </main>
