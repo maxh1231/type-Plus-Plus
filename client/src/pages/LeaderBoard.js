1734b9f19206d40c9352d46733e16ca30f04e8f2
Leithen <leithen113@gmail.com>
2022-02-16 21:45:39 -0700

    Added sub header to leaderboard page

--- client/src/pages/LeaderBoard.js
+++ client/src/pages/LeaderBoard.js
@@ -1,14 +1,32 @@
 import GlobalLeaderBoard from '../components/GlobalLeaderBoard/GlobalLeaderBoard';
 import ActivityLeaderBoard from '../components/ActivityLeaderBoard/ActivityLeaderBoard';
 import WeeklyLeaderBoard from '../components/WeeklyLeaderBoard/';
+import React, { useState } from 'react';
 
 const LeaderBoard = () => {
+    const [activeBoard, setActiveBoard] = useState('global')
+    
     return (
-        <main className="flex-grow">
-            <GlobalLeaderBoard />
-            <ActivityLeaderBoard />
-            <WeeklyLeaderBoard />
-        </main>
+        <section>
+            <div>
+                <ol className='flex justify-around m-2'>
+                    <p onClick={() => setActiveBoard('global')}>Global</p>
+                    <p onClick={() => setActiveBoard('weekly')}>Weekly</p>
+                    <p onClick={() => setActiveBoard('games')}>Games Played</p>
+                </ol>
+            </div>
+            <main className="flex-grow">
+                {activeBoard === 'global' && (
+                    <GlobalLeaderBoard />
+                )}
+                {activeBoard === 'games' && (
+                    <ActivityLeaderBoard />
+                )}
+                {activeBoard === 'weekly' && (
+                    <WeeklyLeaderBoard />
+                )}
+            </main>
+        </section>
     );
 };
 
