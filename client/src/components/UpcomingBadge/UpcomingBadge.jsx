c43cd8c4d6887ee2711d3dcbdaf2a1bd1d73f9a7
Leithen <leithen113@gmail.com>
2022-02-18 15:26:55 -0700

    Copied recent badge to upcoming badge and updated to return an unearned badge:

--- client/src/components/UpcomingBadge/UpcomingBadge.jsx
+++ client/src/components/UpcomingBadge/UpcomingBadge.jsx
@@ -1,9 +1,34 @@
-const UpcomingBadge = () => {
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_BADGES } from '../../utils/queries';
+
+const UpcomingBadge = (runGame) => {
+    const { loading, data, refetch } = useQuery(QUERY_BADGES);
+
+    useEffect(() => {
+        refetch();
+    }, [runGame]) // eslint-disable-line react-hooks/exhaustive-deps
+    
+    if (loading) {
+        return <p>Loading...</p>
+    }
+
+    const badgeArr = data?.badges || [];
+    const activeBadge = badgeArr[badgeArr.length - 1];
 
     return (
-        <>
-            <p>Hello World!</p>
-        </>
+        <div className='container'>
+            {badgeArr.length > 0 ? (
+                <div className='border rounded-md p-2 w-fit m-auto'>
+                    <p className='text-lg text-center border-b'>Unearned Badge:</p>
+                    <img src={activeBadge.placeholder} className='mx-auto my-1' alt='unearned badge'></img>
+                    <p className='text-center font-bold'>{activeBadge.badgeName}</p>
+                    <p className='text-center'>{activeBadge.description}</p>
+                </div>
+            ) : (
+                <p className="text-lg text-center">No badges yet!</p>
+            )}
+        </div>
     )
 }
 
