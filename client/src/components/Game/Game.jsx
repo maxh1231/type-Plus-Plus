d16fc1061ba0dc04065e80abcf72d022c8eda799
Ben H <deepfreespace@gmail.com>
2022-02-18 17:56:17 -0700

    Fixed modal Z-index on game component

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -80,36 +80,45 @@ const Game = ({ sampleArr, unmount, loggedIn }) => {
 
     const endGame = async () => {
         toggleTimer();
-        let badgeData = []
+        let badgeData = [];
         if (userData.length > 0) {
             if (userData.badges.length > 0) {
                 badgeData = [...userData.badges];
             }
         }
-        const userBadges = badgeData.map(badge => badge.badgeName);
-        const newData = { wpm: wpm, accuracy: accuracy, time: timer, errors: errorCount };
+        const userBadges = badgeData.map((badge) => badge.badgeName);
+        const newData = {
+            wpm: wpm,
+            accuracy: accuracy,
+            time: timer,
+            errors: errorCount,
+        };
         // check for badges
         let addedBadge;
         const gameCheck = checkGame(userData.gameCount + 1);
         const scoreCheck = checkScore(newData.wpm);
         const accuracyCheck = checkAccuracy(newData.accuracy);
-        
-        let newBadgeArr = []
+
+        let newBadgeArr = [];
 
         if (gameCheck) {
-            newBadgeArr.push(gameCheck)
-        } if (scoreCheck) {
-            let tmpArr = newBadgeArr
-            newBadgeArr = tmpArr.concat(scoreCheck)
-        } if (accuracyCheck) {
-            let tmpArr = newBadgeArr
-            newBadgeArr = tmpArr.concat(accuracyCheck)
+            newBadgeArr.push(gameCheck);
         }
-        
+        if (scoreCheck) {
+            let tmpArr = newBadgeArr;
+            newBadgeArr = tmpArr.concat(scoreCheck);
+        }
+        if (accuracyCheck) {
+            let tmpArr = newBadgeArr;
+            newBadgeArr = tmpArr.concat(accuracyCheck);
+        }
+
         // create array of new badges to add to the user
-        let earnedBadges = newBadgeArr.filter(badge => !userBadges.includes(badge));
-        console.log(userData)
-        console.log(badgeData)
+        let earnedBadges = newBadgeArr.filter(
+            (badge) => !userBadges.includes(badge)
+        );
+        console.log(userData);
+        console.log(badgeData);
         console.log(newBadgeArr);
         console.log(userBadges);
         console.log(earnedBadges);
@@ -117,7 +126,9 @@ const Game = ({ sampleArr, unmount, loggedIn }) => {
         if (loggedIn) {
             if (earnedBadges.length > 0) {
                 for (let i = 0; i < earnedBadges.length; i++) {
-                    addedBadge = await addBadge({ variables: { badgeName: earnedBadges[i] }})
+                    addedBadge = await addBadge({
+                        variables: { badgeName: earnedBadges[i] },
+                    });
                 }
             }
             try {
@@ -127,7 +138,7 @@ const Game = ({ sampleArr, unmount, loggedIn }) => {
             }
         }
         if (addedBadge) {
-            setModalBadge(addedBadge.data.addBadge)
+            setModalBadge(addedBadge.data.addBadge);
         }
         openModal();
     };
@@ -138,13 +149,15 @@ const Game = ({ sampleArr, unmount, loggedIn }) => {
         for (let i = 0; i < inputText.length; i++) {
             if (inputText[i] !== sampleArr[i]) {
                 // add error styling
-                document.getElementById(i).style.backgroundColor = 'rgba(191, 66, 66, 0.2)';
+                document.getElementById(i).style.backgroundColor =
+                    'rgba(191, 66, 66, 0.2)';
                 document.getElementById(i).style.color = 'red';
                 setValidInput(false);
                 tmpErrorCount++;
             } else {
                 // add correct styling
-                document.getElementById(i).style.backgroundColor = 'rgba(63, 191, 66, 0.1)';
+                document.getElementById(i).style.backgroundColor =
+                    'rgba(63, 191, 66, 0.1)';
                 document.getElementById(i).style.color = 'green';
                 setValidInput(true);
             }
@@ -161,16 +174,28 @@ const Game = ({ sampleArr, unmount, loggedIn }) => {
     const updateUnderline = () => {
         if (inputText.length > 0) {
             try {
-                document.getElementById(inputText.length).style.textDecoration = 'underline';
-                document.getElementById(inputText.length).style.backgroundColor = 'rgba(100, 100, 100, 0.2)';
-                document.getElementById(inputText.length - 1).style.textDecoration = 'none';
-                document.getElementById(inputText.length - 1).style.backgroundColor = 'none';
-                document.getElementById(inputText.length + 1).style.textDecoration = 'none';
-                document.getElementById(inputText.length + 1).style.backgroundColor = 'transparent';
+                document.getElementById(inputText.length).style.textDecoration =
+                    'underline';
+                document.getElementById(
+                    inputText.length
+                ).style.backgroundColor = 'rgba(100, 100, 100, 0.2)';
+                document.getElementById(
+                    inputText.length - 1
+                ).style.textDecoration = 'none';
+                document.getElementById(
+                    inputText.length - 1
+                ).style.backgroundColor = 'none';
+                document.getElementById(
+                    inputText.length + 1
+                ).style.textDecoration = 'none';
+                document.getElementById(
+                    inputText.length + 1
+                ).style.backgroundColor = 'transparent';
             } catch {}
         } else {
             document.getElementById(0).style.textDecoration = 'underline';
-            document.getElementById(0).style.backgroundColor = 'rgba(100, 100, 100, 0.2)';
+            document.getElementById(0).style.backgroundColor =
+                'rgba(100, 100, 100, 0.2)';
             document.getElementById(1).style.textDecoration = 'none';
             document.getElementById(1).style.backgroundColor = 'transparent';
         }
@@ -201,15 +226,14 @@ const Game = ({ sampleArr, unmount, loggedIn }) => {
 
     const openModal = () => {
         setIsOpen(true);
-    }
+    };
 
-    const afterOpenModal = () => {
-    }
+    const afterOpenModal = () => {};
 
     const closeModal = () => {
         setIsOpen(false);
         unmount();
-    }
+    };
 
     return (
         <div id="inputArea" className="m-4">
@@ -256,6 +280,7 @@ const Game = ({ sampleArr, unmount, loggedIn }) => {
                         marginRight: '-50%',
                         transform: 'translate(-50%, -50%)',
                     },
+                    overlay: { zIndex: 100 },
                 }}
             >
                 <div id="modal-container" className="w-fit flex flex-col">
@@ -268,17 +293,21 @@ const Game = ({ sampleArr, unmount, loggedIn }) => {
                         <p>Time: {timer}</p>
                         <p>WPM: {wpm}</p>
                     </div>
-                    {modalBadge && 
-                    <div className='m-auto text-center'>
-                        <h2>You just earned:</h2>
-                        <img src={modalBadge.img} className='m-auto' alt='badge img'></img>
-                        <div>{modalBadge.badgeName}</div>
-                        <div>{modalBadge.description}</div>
-                    </div>
-                    }
+                    {modalBadge && (
+                        <div className="m-auto text-center">
+                            <h2>You just earned:</h2>
+                            <img
+                                src={modalBadge.img}
+                                className="m-auto"
+                                alt="badge img"
+                            ></img>
+                            <div>{modalBadge.badgeName}</div>
+                            <div>{modalBadge.description}</div>
+                        </div>
+                    )}
                     {!loggedIn && (
                         <div className="mx-auto my-6 w-fit">
-                            <Link to='/signup'>
+                            <Link to="/signup">
                                 Log in to save your scores!
                             </Link>
                         </div>
