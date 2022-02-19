e1eb548a84ef555d1e17336e6ce5bf4f8d765dc1
Ben H <deepfreespace@gmail.com>
2022-02-19 16:47:24 -0700

    Card colors

--- client/src/components/Friends/Friends.jsx
+++ client/src/components/Friends/Friends.jsx
@@ -5,15 +5,19 @@ const Friends = ({ friends }) => {
         return null;
     }
     return (
-        <div className="container text-center border w-fit rounded-md ">
-            <h2 className="text-lg text-center border-b">Friends:</h2>
-            {friends.map((friend) => (
-                <button key={friend._id}>
-                    <Link to={`/profile/${friend.username}`}>
-                        {friend.username}
-                    </Link>
-                </button>
-            ))}
+        <div className="block rounded-lg shadow-sm border w-44 text-center transition-all duration-200 dark:border-gray-400">
+            <div className="py-3 px-6 text-lg border-b dark:border-gray-400 rounded-t-lg">
+                Friends
+            </div>
+            <div className="p-1">
+                {friends.map((friend) => (
+                    <button key={friend._id}>
+                        <Link to={`/profile/${friend.username}`}>
+                            {friend.username}
+                        </Link>
+                    </button>
+                ))}
+            </div>
         </div>
     );
 };
--- client/src/components/RecentBadge/RecentBadge.jsx
+++ client/src/components/RecentBadge/RecentBadge.jsx
@@ -4,32 +4,42 @@ import { QUERY_MYBADGE } from '../../utils/queries';
 
 const RecentBadge = ({ runGame }) => {
     const { loading, data, refetch } = useQuery(QUERY_MYBADGE);
-    
+
     useEffect(() => {
         refetch();
-    }, [runGame]) // eslint-disable-line react-hooks/exhaustive-deps
-    
+    }, [runGame]); // eslint-disable-line react-hooks/exhaustive-deps
+
     if (loading) {
-        return <p>Loading...</p>
+        return <p>Loading...</p>;
     }
 
     const badgeArr = data?.meBadges.badges || [];
     const activeBadge = badgeArr[badgeArr.length - 1];
 
     return (
-        <div className='container'>
+        <div className="flex justify-center transition-all duration-200">
             {badgeArr.length > 0 ? (
-                <div className='border rounded-md p-2 w-fit m-auto'>
-                    <p className='text-lg text-center border-b'>Recent Badge:</p>
-                    <img src={activeBadge.img} className='mx-auto my-1' alt='recent badge'></img>
-                    <p className='text-center font-bold'>{activeBadge.badgeName}</p>
-                    <p className='text-center'>{activeBadge.description}</p>
+                <div className="block rounded-lg w-44 shadow-sm border max-w-sm text-center dark:border-gray-400">
+                    <div className="py-3 px-6 text-lg border-b dark:border-gray-400">
+                        Recent Badge
+                    </div>
+                    <img
+                        src={activeBadge.img}
+                        className="mx-auto my-1"
+                        alt="recent badge"
+                    ></img>
+                    <div className="p-1">
+                        <h5 className="text-lg mb-2">
+                            {activeBadge.badgeName}
+                        </h5>
+                        <p className="mb-4">{activeBadge.description}</p>
+                    </div>
                 </div>
             ) : (
                 <p className="text-lg text-center">No badges yet!</p>
             )}
         </div>
-    )
+    );
 };
 
 export default RecentBadge;
