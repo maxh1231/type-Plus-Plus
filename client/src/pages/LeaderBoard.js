3982e8dda92a243f0889ba2ed89a56dd197c40b3
Leithen <leithen113@gmail.com>
2022-02-20 12:34:38 -0700

    Restyled tabs on leaderboard page

--- client/src/pages/LeaderBoard.js
+++ client/src/pages/LeaderBoard.js
@@ -8,28 +8,19 @@ const LeaderBoard = () => {
 
     return (
         <main className="flex-grow flex flex-col items-center text-gray-700 dark:text-gray-400 dark:bg-gray-800">
-            <div className="grid grid-cols-3 container my-5">
-                <p className="text-center">
-                    <span
-                        className="cursor-pointer  text-lg hover:text-theme-red transition-all duration-300"
-                        onClick={() => setActiveBoard('global')}
-                    >
+            <div className="grid grid-cols-3 container my-5 h-10 items-center">
+                <p className={`text-center bg-mid-gray h-full pt-1 rounded-t-xl border-x border-t hover:text-theme-red hover:opacity-80 cursor-pointer ${activeBoard === 'global' && 'bg-inherit text-theme-red'}`} onClick={() => setActiveBoard('global')}>
+                    <span className="text-lg transition-all duration-300" >
                         Global
                     </span>
                 </p>
-                <p className="text-center">
-                    <span
-                        className="cursor-pointer  text-lg hover:text-theme-red transition-all duration-300"
-                        onClick={() => setActiveBoard('weekly')}
-                    >
+                <p className={`text-center bg-mid-gray h-full pt-1 rounded-t-xl border-x border-t hover:text-theme-red hover:opacity-80 cursor-pointer ${activeBoard === 'weekly' && 'bg-inherit text-theme-red'}`} onClick={() => setActiveBoard('weekly')}>
+                    <span className="text-lg transition-all duration-300" >
                         Weekly
                     </span>
                 </p>
-                <p className="text-center">
-                    <span
-                        className="cursor-pointer  text-lg hover:text-theme-red transition-all duration-300"
-                        onClick={() => setActiveBoard('games')}
-                    >
+                <p className={`text-center bg-mid-gray h-full pt-1 rounded-t-xl border-x border-t hover:text-theme-red hover:opacity-80 cursor-pointer ${activeBoard === 'games' && 'bg-inherit text-theme-red'}`} onClick={() => setActiveBoard('games')}>
+                    <span className="text-lg transition-all duration-300" >
                         Games Played
                     </span>
                 </p>
