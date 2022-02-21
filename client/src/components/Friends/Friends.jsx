ea91cd7379cdd978fe7c9fcbd37f472d5bdfaf85
maxh1231 <maxhumpherys@gmail.com>
2022-02-21 14:45:16 -0700

    friend profile pics

--- client/src/components/Friends/Friends.jsx
+++ client/src/components/Friends/Friends.jsx
@@ -4,6 +4,8 @@ const Friends = ({ friends }) => {
     if (!friends || !friends.length) {
         return null;
     }
+
+    console.log(friends)
     return (
         <div className="block rounded-lg shadow-sm border w-44 text-center transition-all duration-200 dark:border-gray-400 h-60">
             <div className="py-3 px-6 text-lg border-b bg-gray-100 dark:bg-gray-900 dark:border-gray-400 rounded-t-lg">
@@ -12,7 +14,9 @@ const Friends = ({ friends }) => {
             <div className="p-1 h-44">
                 <div className="flex flex-col flex-1 overflow-y-auto h-44">
                     {friends.map((friend) => (
+
                         <span key={friend._id}>
+                            <img src={friend.profilePic}></img>
                             <Link
                                 to={`/profile/${friend.username}`}
                                 className="hover:text-theme-red dark:hover:text-theme-red transition-all duration-300"
