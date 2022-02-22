a552c5570f111449eff68b9ce03bbd726b29d2ed
Leithen <leithen113@gmail.com>
2022-02-21 17:31:50 -0700

    Adde breakpoints for large devices

--- client/src/components/BadgeList/BadgeList.jsx
+++ client/src/components/BadgeList/BadgeList.jsx
@@ -86,21 +86,21 @@ const BadgeList = ({view}) => {
             {viewGrid ? (
                 <div className='flex flex-wrap w-full p-4'>
                     <p className='text-xl font-bold text-center w-full text-gray-600 dark:text-gray-200 p-2'>Earned</p>
-                    <div className='w-full grid grid-cols-2 gap-4'>
+                    <div className='w-full grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5'>
                         {myBadgeArr.map(badge => (
-                            <div id='card' key={uuid()} className='border rounded-md p-2 w-full'>
+                            <div id='card' key={uuid()} className='border rounded-md p-2 w-full bg-gray-100 dark:bg-inherit shadow-md'>
                                 <img src={`.${badge.img}`} key={uuid()} className='m-auto' alt='badge'></img>
-                                <p key={uuid()} className='p-1 text-center font-bold'>{badge.badgeName}</p>
+                                <p key={uuid()} className='p-1 text-center font-bold dark:text-gray-200'>{badge.badgeName}</p>
                                 <p key={uuid()} className='p-1 text-center italic'>{badge.description}</p>
                             </div>
                         ))}
                     </div>
                     <p className='text-xl font-bold text-center w-full text-gray-600 dark:text-gray-200 p-2'>Unearned</p>
-                    <div className='grid w-full grid-cols-2 gap-4'>
+                    <div className='grid w-full grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5'>
                         {tmpArr.map(badge => (
-                            <div id='card' key={uuid()} className='border rounded-md p-2 w-full flex flex-col justify-between'>
+                            <div id='card' key={uuid()} className='border rounded-md p-2 w-full flex flex-col justify-between bg-gray-100 dark:bg-inherit shadow-md'>
                                 <img src={`.${badge.placeholder}`} key={uuid()} className='m-auto' alt='badge'></img>
-                                <p key={uuid()} className='p-1 text-center font-bold'>{badge.badgeName}</p>
+                                <p key={uuid()} className='p-1 text-center font-bold dark:text-gray-200'>{badge.badgeName}</p>
                                 <p key={uuid()} className='p-1 text-center italic'>{badge.description}</p>
                                 <div className='w-20 h-20 m-auto bottom-0'>
                                     <CircularProgressbar value={Math.round(renderProgress(badge.category)/badge.targetVal * 100)} text={`${Math.round(renderProgress(badge.category)/badge.targetVal * 100)}%`} />
@@ -114,10 +114,10 @@ const BadgeList = ({view}) => {
                     <div className='w-full justify-center'>
                         <p className='text-xl font-bold m-2 text-gray-600 dark:text-gray-200'>Earned</p>
                         {myBadgeArr.map(badge => (
-                            <div id='card' key={uuid()} className='flex border rounded-md p-2 m-2'>
+                            <div id='card' key={uuid()} className='flex border rounded-md p-2 m-4 bg-gray-100 dark:bg-inherit shadow-md'>
                                 <img src={`.${badge.img}`} key={uuid()} className='p-2 h-max my-auto' alt='badge'></img>
                                 <div className='p-1 border-l'>
-                                    <p key={uuid()} className='p-2 font-bold block'>{badge.badgeName}</p>
+                                    <p key={uuid()} className='p-2 font-bold block dark:text-gray-200'>{badge.badgeName}</p>
                                     <p key={uuid()} className='p-2 italic block'>{badge.description}</p>
                                 </div>
                             </div>
@@ -126,17 +126,17 @@ const BadgeList = ({view}) => {
                     <div className='w-full justify-center'>
                         <p className='text-xl font-bold m-2 text-gray-600 dark:text-gray-200'>Not Yet Earned</p>
                         {tmpArr.map(badge => (
-                            <div id='card' key={uuid()} className='border rounded-md p-2 m-2'>
+                            <div id='card' key={uuid()} className='border rounded-md p-2 m-4 bg-gray-100 dark:bg-inherit shadow-md'>
                                 <div className='flex'>
                                     <img src={`.${badge.placeholder}`} key={uuid()} className='p-2 h-max my-auto' alt='badge'></img>
                                     <div className='p-1 border-l'>
-                                        <p key={uuid()} className='p-2 font-bold block'>{badge.badgeName}</p>
+                                        <p key={uuid()} className='p-2 font-bold block dark:text-gray-200'>{badge.badgeName}</p>
                                         <p key={uuid()} className='p-2 italic block'>{badge.description}</p>
                                     </div>
                                 </div>
-                                <p>Progress: {renderProgress(badge.category)}/{badge.targetVal}</p>
-                                <div className="w-full bg-gray-200 rounded-full">
-                                    <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{width: `${renderProgress(badge.category)/badge.targetVal * 100}%`}}>{Math.floor(renderProgress(badge.category)/badge.targetVal * 100)}%</div>
+                                <p className='mt-2'>Progress: {renderProgress(badge.category)}/{badge.targetVal}</p>
+                                <div className="w-full bg-gray-300 rounded-full">
+                                    <div className="bg-blue-600 text-xs font-bold text-black text-center p-0.5 leading-none rounded-l-full" style={{width: `${renderProgress(badge.category)/badge.targetVal * 100}%`}}>{Math.floor(renderProgress(badge.category)/badge.targetVal * 100)}%</div>
                                 </div>
                             </div>
                         ))}
