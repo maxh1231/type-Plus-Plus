68a0a1b2f3a3ef8b8182deb1df7ee75ec858d504
Ben H <deepfreespace@gmail.com>
2022-02-21 20:21:55 -0700

    Icon animations for badge page

--- client/src/components/BadgeList/BadgeList.jsx
+++ client/src/components/BadgeList/BadgeList.jsx
@@ -75,16 +75,32 @@ const BadgeList = ({ view }) => {
 
     if (loading) {
         return (
-            <div className='m-auto text center w-fit pt-6'>
+            <div className="m-auto text center w-fit pt-6">
                 <div className="inline-flex items-center w-fit px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-theme-blue transition ease-in-out duration-150">
-                    <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    <svg
+                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            stroke-width="4"
+                        ></circle>
+                        <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
                     </svg>
-                Loading...
+                    Loading...
                 </div>
             </div>
-        )
+        );
     }
 
     return (
@@ -96,7 +112,8 @@ const BadgeList = ({ view }) => {
                 >
                     <ViewGridIcon
                         className={`h-5 w-5 inline ${
-                            viewGrid && 'stroke-theme-red'
+                            viewGrid &&
+                            'stroke-theme-red transition-all duration-300'
                         }`}
                     />
                 </button>
@@ -106,7 +123,8 @@ const BadgeList = ({ view }) => {
                 >
                     <ViewListIcon
                         className={`h-5 f-2 inline ${
-                            !viewGrid && 'stroke-theme-red'
+                            !viewGrid &&
+                            'stroke-theme-red transition-all duration-300'
                         }`}
                     />
                 </button>
