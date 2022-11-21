16bd6805495ee1b99b8d12d04c5cec9654944c2d
Max Humpherys <maxhumpherys@gmail.com>
2022-11-21 15:54:49 -0700

    changed mongo URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/type-plus-plus', {
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.7pgk9ri.mongodb.net/?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
